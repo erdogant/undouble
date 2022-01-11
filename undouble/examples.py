63255a63c7e25b6aa56d1a0df9ab8ed90fe3f133
erdogant <erdogant@gmail.com>
2022-01-11 23:44:24 +0100

    update

--- undouble/examples.py
+++ undouble/examples.py
@@ -1,19 +1,22 @@
 # https://content-blockchain.org/research/testing-different-image-hash-functions/
 
 # %%
-import undouble
-print(dir(undouble))
-print(undouble.__version__)
-
-# %%
 from undouble import Undouble
-
-model = Undouble(targetdir='//NAS_SYNOLOGY//Photo//2016')
-# model = Undouble(targetdir='D://magweg1//2003')
-model.preprocessing()
+# import undouble
+# print(dir(undouble))
+# print(undouble.__version__)
 
 # %%
-model.fit(method='phash')
+
+targetdir='//NAS_SYNOLOGY//Photo//2016'
+targetdir='D://magweg//101_ObjectCategories//bonsai'
+
+# %%
+model = Undouble(method='phash', grayscale=True)
+model.preprocessing(targetdir)
+model.fit()
+
+# %%
 model.find(score=5)
 
 # %%
@@ -26,4 +29,3 @@ model.move()
 model.results
 
 # %%
-
