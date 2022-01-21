5c0f20adf061ab3bd0949ec6ae41d76ec1892a4e
erdogant <erdogant@gmail.com>
2022-01-21 09:44:58 +0100

    1.0.0

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setuptools.setup(
      license_files=["LICENSE"],
      classifiers=[
          "Programming Language :: Python :: 3",
-         "License :: OSI Approved :: BSD 3-Clause License",
+         "License :: OSI Approved :: 3-clause BSD License",
          "Operating System :: OS Independent",
      ],
  )
