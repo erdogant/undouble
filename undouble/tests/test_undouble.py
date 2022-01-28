9faa6444c45747bcde83df3c5a94ff947e7231fa
erdogant <erdogant@gmail.com>
2022-01-28 13:58:25 +0100

    update unit test

--- undouble/tests/test_undouble.py
+++ undouble/tests/test_undouble.py
@@ -2,156 +2,80 @@ from undouble import Undouble
 import itertools as it
 import numpy as np
 import unittest
+from tqdm import tqdm
 
 class TestUNDOUBLE(unittest.TestCase):
 
     def test_import_data(self):
-        cl = Undouble()
+        model = Undouble()
         # Import flowers example
-        X = cl.import_example(data='flowers')
+        X = model.import_example(data='flowers')
 
         # Check numpy array imports
-        assert cl.import_data(np.array(X))
-        assert cl.import_data(X[0])
-        assert cl.import_data([X[0]])
+        model.import_data(X)
+        assert model.results['img'].shape==(214, 128, 128, 3)
+        assert len(model.results['pathnames'])==214
+        assert len(model.results['filenames'])==214
+        assert set(model.results.keys())==set(['img', 'feat', 'pathnames', 'filenames'])
 
-        # Check output
-        cl = Clustimage(dim=(128, 128), grayscale=False)
-        _ =  cl.import_data(X)
-        assert np.all(np.isin([*cl.results.keys()], ['img', 'feat', 'xycoord', 'pathnames', 'labels', 'filenames']))
-        assert cl.results['img'].shape==(214, 49152)
-        # Check grayscale parameter with imports
-        cl = Clustimage(dim=(128,128), grayscale=True)
-        _ = cl.import_data(X)
-        assert cl.results['img'].shape==(214, 16384)
-
-        # Import mnist example
-        X = cl.import_example(data='mnist')
-        cl = Clustimage()
-        _ = cl.import_data(X)
-        assert np.all(np.isin([*cl.results.keys()], ['img', 'feat', 'xycoord', 'pathnames', 'labels', 'filenames']))
-        assert cl.results['img'].shape==(1797, 64)
-        assert len(cl.results['pathnames'])==X.shape[0]
-        assert len(cl.results['filenames'])==X.shape[0]
-
-    def test_extract_feat(self):
-        cl = Clustimage(method='pca')
+    def test_compute_imghash(self):
+        model = Undouble()
         # Import flowers example
-        X = cl.import_example(data='flowers')
-        X = cl.import_data(X)
-        _ = cl.extract_feat(X)
-        assert cl.results['feat'].shape==(X['img'].shape[0], 153)
+        X = model.import_example(data='flowers')
+        imgs = model.import_data(X, return_results=True)
 
-        # Init with settings such as PCA
-        cl = Clustimage(method='hog', verbose=50)
-        # load example with flowers
-        pathnames = cl.import_example(data='flowers')
-        # Cluster flowers
-        results = cl.compute_hash(pathnames)
-        # Read the unseen image. Note that the find functionality also performs exactly the same preprocessing steps as for the clustering.
-        results_find = cl.find(pathnames[0:2], k=0, alpha=0.05)
+        hash_sizes=[4,8,16]
+        for hash_size in hash_sizes:
+            hashs = model.compute_imghash(imgs['img'][0], to_array=True, hash_size=hash_size)
+            assert len(hashs[0])==(hash_size*hash_size)
 
+        hashs = model.compute_imghash(imgs['img'][0:5], to_array=True, hash_size=8)
+        assert len(hashs)==5
+        assert hashs[0].shape==(64,)
+        hashs = model.compute_imghash(imgs['img'][0:5], to_array=False, hash_size=8)
+        assert len(hashs)==5
+        assert hashs[0].shape==(8,8)
 
-    def test_embedding(self):
-        cl = Clustimage(method='pca')
-        # Import flowers example
-        X = cl.import_example(data='flowers')
-        X = cl.import_data(X)
-        Xfeat = cl.extract_feat(X)
-        _ = cl.embedding(Xfeat)
-        assert cl.results['xycoord'].shape==(X['img'].shape[0], 2)
-
-    def test_embedding(self):
-        cl = Clustimage(method='pca')
-        # Import flowers example
-        X = cl.import_example(data='flowers')
-        X = cl.import_data(X)
-        Xfeat = cl.extract_feat(X)
-        xycoord = cl.embedding(Xfeat)
-        labels = cl.cluster()
-        assert len(cl.results['labels'])==X['img'].shape[0]
-
-    def test_cluster(self):
-        cl = Clustimage()
-        X = cl.import_example(data='flowers')
-        results = cl.fit_transform(X)
-        assert np.all(np.isin([*cl.results.keys()], ['img', 'feat', 'xycoord', 'pathnames', 'filenames', 'labels']))
-        assert len(cl.cluster())==len(X)
-    
-        # Parameters combinations to check
-        param_grid = {
-        	'cluster_space':['high','low'],
-        	'cluster':['agglomerative'],
-        	'evaluate' : ['silhouette', 'dbindex'],
-            'min_clust' : [2, 4, 6],
-            'max_clust' : [10, 20, 30],
-        	}
-        # Make the combinatinos
-        allNames = param_grid.keys()
-        combinations = list(it.product(*(param_grid[Name] for Name in allNames)))
-        for combination in combinations:
-            labx = cl.cluster(cluster_space=combination[0], cluster=combination[1], evaluate=combination[2], metric='euclidean', linkage='ward', min_clust=combination[3], max_clust=combination[4])
-            assert len(labx)==len(X)
-    
-    def test_find(self):
-        cl = Clustimage(method='pca', grayscale=False)
-        # load example with flowers
-        path_to_imgs = cl.import_example(data='flowers')
-        # Extract features (raw images are not stored and handled per-image to save memory)
-        results = cl.compute_hash(path_to_imgs, min_clust=10)
-        # Check nr. of features
-        featshape = cl.results['feat'].shape
-        # Predict
-        results_find = cl.find(path_to_imgs[0:5], k=None, alpha=0.05)
-        assert  cl.results['feat'].shape==featshape
-        # assert np.all(np.isin([*results_find.keys()], ['feat', '0001.png', '0002.png', '0003.png', '0004.png', '0005.png']))
-        # assert len(results_find['0001.png']['y_idx'])>=1
-        # assert len(results_find['0002.png']['y_idx'])>=1
-        # assert len(results_find['0003.png']['y_idx'])>=30
-        # assert len(results_find['0004.png']['y_idx'])>=1
-        # assert len(results_find['0005.png']['y_idx'])>=1
-
-        # results_find = cl.find(path_to_imgs[0:5], k=1, alpha=None)
-        # assert len(results_find['0001.png']['y_idx'])==1
-        # assert len(results_find['0002.png']['y_idx'])==1
-        # assert len(results_find['0003.png']['y_idx'])==1
-        # assert len(results_find['0004.png']['y_idx'])==1
-        # assert len(results_find['0005.png']['y_idx'])==1
-
-    def test_predict(self):
-        # Init
-        cl = Clustimage(method='pca', grayscale=True, params_pca={'n_components':14})
-        # Load example with faces
-        X = cl.import_example(data='flowers')
-        # Cluster
-        results = cl.compute_hash(X)
-        assert np.all(np.isin([*cl.results.keys()], ['img', 'feat', 'xycoord', 'pathnames', 'filenames', 'labels']))
+        hashs = model.compute_imghash(imgs['img'][0], to_array=True, hash_size=8)
+        assert len(hashs)==1
+        assert hashs[0].shape==(64,)
+        hashs = model.compute_imghash(imgs['img'][0], to_array=False, hash_size=8)
+        assert len(hashs)==1
+        assert hashs[0].shape==(8,8)
 
     def test_compute_hash(self):
-        # Example data
-        cl = Clustimage()
-        Xflowers = cl.import_example(data='flowers')
-        Xflowers=Xflowers[0:50]
-        Xdigits = cl.import_example(data='mnist')
-        Xdigits=Xdigits[0:50,:]
-        Xfaces = cl.import_example(data='faces')
-        Xfaces=Xfaces[0:50,:]
+        model = Undouble(method='phash')
+        # Import flowers example
+        X = model.import_example(data='flowers')
+        # Import data
+        model.import_data(X, return_results=False)
+        # Compute Hash
+        model.compute_hash()
+        assert set(model.results.keys())==set(['img', 'pathnames', 'filenames', 'img_hash_bin', 'img_hash_hex', 'adjmat'])
+        assert np.all(model.results['img_hash_bin'][0] == [ True, False, False,  True, False,  True, False,  True,  True,
+                                                           False, False,  True,  True,  True, False, False, False,  True,
+                                                           True,  True, False, False,  True, False, False,  True,  True,
+                                                           False, False, False,  True, False, False,  True,  True,  True,
+                                                           True,  True, False, False,  True, False, False, False,  True,
+                                                           False,  True,  True, False,  True,  True, False,  True, False,
+                                                           True,  True, False, False,  True,  True,  True, False, False,
+                                                           False])
 
-        # Parameters combinations to check
         param_grid = {
-        	'method':['ahash', 'pca', 'hog', None],
-        	'embedding':['tsne', None],
-        	'cluster_space' : ['high', 'low'],
-        	'grayscale' : [True, False],
-            'dim' : [(8,8), (128,128), (256,256)],
-            'data' : [Xflowers, Xdigits]
+        	'method': ['ahash','phash','dhash','whash-haar'],#,'crop-resistant'],
+        	'grayscale':[True, False],
+        	'hash_size' : [4, 8, 16],
+            'dim' : [(64,64), (128,128), (256,256)]
         	}
-        # Make the combinatinos
+
         allNames = param_grid.keys()
-        combinations = list(it.product(*(param_grid[Name] for Name in allNames)))
-        # Iterate over all combinations
-        for i, combination in enumerate(combinations):
-            # init
-            cl = Clustimage(method=combination[0], embedding=combination[1], grayscale=combination[3], dim=combination[4], verbose=30, params_pca={'n_components':50})
-            # Preprocessing and feature extraction
-            assert cl.compute_hash(combination[5], cluster_space=combination[2])
+        combinations = it.product(*(param_grid[Name] for Name in allNames))
+        combinations=list(combinations)
+
+        for combination in tqdm(combinations):
+            model = Undouble(method=combination[0], grayscale=combination[1], hash_size=combination[2], dim=combination[3], verbose=40)
+            # Import data
+            model.import_data(X, return_results=False)
+            # Compute Hash
+            assert model.compute_hash(return_dict=True)
+
