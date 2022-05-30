a9157c4a57329b55486a15d607057a4c9e455395
erdogant <erdogant@gmail.com>
2022-05-30 17:01:14 +0200

    1.2.4

--- undouble/__init__.py
+++ undouble/__init__.py
@@ -12,7 +12,7 @@ from undouble.undouble import (
 
 __author__ = 'Erdogan Tasksen'
 __email__ = 'erdogant@gmail.com'
-__version__ = '1.2.3'
+__version__ = '1.2.4'
 
 # module level doc-string
 __doc__ = """
