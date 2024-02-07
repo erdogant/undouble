d5123f6910be2de950a67712bcb0c0cf986b70f5
erdogant <erdogant@gmail.com>
2024-02-07 19:31:11 +0100

    update setup and gitignore

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ else:
     raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
 
 # Setup ------------
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding="utf8") as fh:
     long_description = fh.read()
 setuptools.setup(
      install_requires=['matplotlib','numpy','tqdm', 'clustimage>=1.5.19','ismember','datazets>=0.1.7'],
