a03fd9727110c2563a4f623f504e2bcc87dd268d
erdogant <erdogant@gmail.com>
2025-04-23 21:51:32 +0200

    udpate

--- undouble/__init__.py
+++ undouble/__init__.py
@@ -17,7 +17,7 @@ __version__ = '1.4.10'
 # Setup root logger
 _logger = logging.getLogger('undouble')
 _log_handler = logging.StreamHandler()
-_fmt = '[{asctime}] [{name}] {msg}'
+_fmt = '[{asctime}] [{name}] [{levelname}] {msg}'
 _formatter = logging.Formatter(fmt=_fmt, style='{', datefmt='%d-%m-%Y %H:%M:%S')
 _log_handler.setFormatter(_formatter)
 _log_handler.setLevel(logging.DEBUG)
