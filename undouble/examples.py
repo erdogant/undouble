513cf9065e2a0ebb782c2aa5841990e95604385f
erdogant <erdogant@gmail.com>
2025-12-28 17:18:59 +0100

    uppdate example

--- undouble/examples.py
+++ undouble/examples.py
@@ -1,8 +1,183 @@
-from undouble import Undouble
+# from undouble import Undouble
 # import undouble
 # print(dir(undouble))
 # print(undouble.__version__)
 
+# Import library
+from clustimage import Clustimage
+import numpy as np
+from matplotlib.colors import ListedColormap
+from scatterd import scatterd
+cmap = ListedColormap(["blue", "red"])
+
+# init
+method = 'hog'
+cl = Clustimage(method=method, params_hash={'threshold': 0, 'hash_size': 16},)
+# ahash
+# phash
+# dhash
+# whash-haar
+# whash-db4
+# colorhash
+
+# Note that you manually need to download the data from the caltech website and simply provide the directory where the images are stored.
+# Download dataset
+
+# Cluster  images in path location.
+targetdir = r'C://101_ObjectCategories//'
+results = cl.fit_transform(targetdir, min_clust=1, max_clust=20)
+
+# If you want to experiment with a different clustering and/or evaluation approach, use the cluster functionality.
+# This will avoid pre-processing, and performing the feature extraction of all images again.
+# You can also cluster on the 2-D embedded space by setting the cluster_space parameter 'low'
+#
+# cluster(cluster='agglomerative', evaluate='dbindex', metric='euclidean', linkage='ward', min_clust=15, max_clust=200, cluster_space='high')
+
+# Cluster evaluation plots such as the Silhouette plot
+cl.clusteval.plot(title=f'Method: {method}')
+cluster_labels = cl.results['labels']
+y=np.array(list(map(lambda x: r'\fake' in x, cl.results['pathnames'])))
+cl.results['labels'] = y.astype(int)
+# cl.clusteval.scatter(cl.results['xycoord'], s=100)
+
+scatterd(x=cl.results['xycoord'][:,0], y=cl.results['xycoord'][:,1], labels=y, cmap=cmap, title=f'Method: {method}')
+
+# %%
+
+
+# PCA explained variance plot
+
+cl.pca.plot()
+
+# Dendrogram
+cl.dendrogram()
+
+# Plot unique image per cluster
+cl.plot_unique(img_mean=False)
+
+# Scatterplot
+cl.scatter(dotsize=50, zoom=0.2, img_mean=False)
+
+# Plot images per cluster or all clusters
+cl.plot(labels=8)
+
+# %%
+
+# Import library
+from undouble import Undouble
+
+# Initialize model
+model = Undouble(method='phash', hash_size=8)
+
+# Download dataset here: http://www.vision.caltech.edu/Image_Datasets/Caltech101/101_ObjectCategories.tar.gz
+# targetdir = 'C://101_ObjectCategories'
+targetdir = r'C:/101_ObjectCategories//'
+# from pathlib import Path
+# targetdir.exists()
+
+# Importing the files files from disk, cleaning and pre-processing
+model.import_data(targetdir, use_thumbnail_cache=False)
+
+# Compute image-hash
+model.compute_hash()
+
+# [undouble] >INFO> Extracting images from: [C://101_ObjectCategories]
+# [undouble] >INFO> [9144] files are collected recursively from path: [D://101_ObjectCategories]
+# [undouble] >INFO> [9144] images are extracted.
+# [undouble] >INFO> Reading and checking images.
+# [undouble] >INFO> Reading and checking images.
+# 100%|██████████| 9144/9144 [02:14<00:00, 67.95it/s] 
+# 100%|██████████| 9144/9144 [00:02<00:00, 3514.21it/s]
+# [undouble] >INFO> Compute adjacency matrix [9144x9144] with absolute differences based on the image-hash of [phash].
+
+# Group images that are identical in image-hash, i.e. those with a hash difference of 0.
+model.group(threshold=0)
+
+# [undouble] >INFO> [38] groups with similar image-hash.
+# [undouble] >INFO> [38] groups are detected for [79] images.
+
+# print([*model.results.keys()])
+#   * img: Preprocessed images
+#   * pathnames: Absolute path location to image file
+#   * filenames: Filename
+#   * img_hash_bin: binary image hash
+#   * img_hash_hex: hex image hash
+#   * adjmat: adjacency matrix containing absolute hash differences between the images
+#   * select_pathnames: Selected path locations that have image-hash score <= threshold
+#   * select_scores: Image-hash scores of the selected images
+#   * stats: Some simple statistics about the number of groups and imges detected.
+
+# Plot the marked images
+model.plot()
+
+# Move the files
+model.move_to_dir()
+
+# -------------------------------------------------
+# >You are at the point of physically moving files.
+# -------------------------------------------------
+# >[79] similar images are detected over [38] groups.
+# >[41] images will be moved to the [undouble] directory.
+# >[38] images will be copied to the [undouble] directory.
+
+# >[C]ontinue moving all files.
+# >[W]ait in each directory.
+# >[Q]uit
+# >Answer: W
+
+# [undouble] >INFO> Working in dir: [D://101_ObjectCategories/BACKGROUND_Google]
+# ><enter> to proceed to the next directory.
+# >[C]ontinue to move all files.
+# >[Q]uit.
+# >Answer:
+    
+
+
+# %%
+
+import cv2
+from scipy.spatial import distance
+import numpy as np
+import matplotlib.pyplot as plt
+from undouble import Undouble
+
+methods = ['ahash', 'dhash', 'whash-haar']
+
+for method in methods:
+    # Average Hash
+    model = Undouble(method=method, hash_size=8)
+    # Import example data
+    targetdir = model.import_example(data='cat_and_dog')
+    # Grayscaling and scaling
+    model.import_data(targetdir)
+    # Compute image for only the first image.
+    hashs = model.compute_imghash(model.results['img'][0], to_array=False)
+    # Compute the image-hash
+    image_hash = ''.join(hashs[0].astype(int).astype(str).ravel())
+    print(f'{method } Hash:')
+    print(f"Binary image hash: {image_hash}")
+    print(f"Hex image hash: {(hex(int(image_hash, 2)))}")
+
+    # Import image for plotting purposes
+    img_g = cv2.imread(model.results['pathnames'][0], cv2.IMREAD_GRAYSCALE)
+    img_r = cv2.resize(img_g, (8, 8), interpolation=cv2.INTER_AREA)
+
+    # Make the figure
+    fig, ax = plt.subplots(2, 2, figsize=(15, 10))
+    ax[0][0].imshow(model.results['img'][0][..., ::-1])
+    ax[0][0].axis('off')
+    ax[0][0].set_title('Source image')
+    ax[0][1].imshow(img_g, cmap='gray')
+    ax[0][1].axis('off')
+    ax[0][1].set_title('grayscale image')
+    ax[1][0].imshow(img_r, cmap='gray')
+    ax[1][0].axis('off')
+    ax[1][0].set_title('grayscale image, size %.0dx%.0d' %(8, 8))
+    ax[1][1].imshow(hashs[0], cmap='gray')
+    ax[1][1].axis('off')
+    ax[1][1].set_title(method + ' function')
+    
+    
 # %% issue
 from undouble import Undouble
 
